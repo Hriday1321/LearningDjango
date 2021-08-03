caf8bc33eac365bad3f59405fd4bbfdffa077e27
EC2 Default User <hridaygupta100@hotmail.com>
2021-08-03 08:33:14 +0000

    routes setup

--- FirstProject/home/views.py
+++ FirstProject/home/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import logout
 
 # Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+    
+def login(request):
+    user = None
+    userName = None
+    passw = None
+    if request.method=="POST":
+        userName=request.POST.get('username')
+        passw=request.POST.get('password')
+    user = authenticate(username=userName, password=passw)
+    print(user)
+    if user is not None:
+        return redirect('/')
+    else:
+        return render(request, "login.html")
+    return render(request, "login.html")
+    
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
